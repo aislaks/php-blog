00bf6232a897b9240d59871740c68b0e632f645f
apai <aiswaryalakshmiu.pai@factset.com>
2020-08-06 21:06:39 +0530

    first commit

---
+++ data.php
@@ -0,0 +1,25 @@
+<?php
+
+$menus = [
+    ["title" => "Home", "url" => "/"],
+    ["title" => "About", "url" => "#about"],
+    ["title" => "Gallery", "url" => "#gallery"],
+    ["title" => "Contact", "url" => "#contact"]
+];
+
+$posts = [
+  [
+    "title" => "Demo title 1",
+    "intro" => "This is simple content and its cool",
+    "image" => "https://picsum.photos/150/150/",
+    "author" => "Author 1",
+    "date" => "Date 1"
+  ],
+  [
+    "title" => "Demo title 2",
+    "intro" => "This is simple content as second article and its cool",
+    "image" => "https://picsum.photos/150/150/",
+    "author" => "Author 2",
+    "date" => "Date 1"
+  ]
+];
